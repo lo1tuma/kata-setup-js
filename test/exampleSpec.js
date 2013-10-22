5d0aca9e5532f389cddc04a17d54f205d97be10f
Mathias Schreck <mat@lusini.com>
2013-10-22 16:15:07 +0200

    implement two copies of one book

--- test/exampleSpec.js
+++ test/exampleSpec.js
@@ -1,11 +1,15 @@
 var expect = require('chai').expect;
 
-function priceFor() {
-    return 8;
+function priceFor(cart) {
+    return cart.book1 * 8;
 }
 
 describe('KataPotter', function () {
     it('charges 8 EUR for a single book', function () {
         expect(priceFor({ book1: 1})).to.equal(8);
+    });
+
+    it('charges 16 EURO fro two copies of the same book', function () {
+        expect(priceFor({book1: 2})).to.equal(16);
     });
 });
