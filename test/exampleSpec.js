f014b66172834817d666db47444c31ef381f4eff
Mathias Schreck <mat@lusini.com>
2013-10-22 16:25:19 +0200

    added first support for discounts

--- test/exampleSpec.js
+++ test/exampleSpec.js
@@ -1,7 +1,14 @@
 var expect = require('chai').expect;
 
 function priceFor(cart) {
-    return cart.book1 * 8;
+    var differentCategories = Object.keys(cart).length,
+        discountFor2Products = 0.95;
+
+    if(differentCategories > 1) {
+        return discountFor2Products * (2 * 8);
+    } else {
+        return cart.book1 * 8;
+    }
 }
 
 describe('KataPotter', function () {
@@ -11,5 +18,9 @@ describe('KataPotter', function () {
 
     it('charges 16 EURO fro two copies of the same book', function () {
         expect(priceFor({book1: 2})).to.equal(16);
+    });
+
+    it('charges 95 % of 16 EUR for two separate books', function () {
+        expect(priceFor({book1: 1, book2: 1})).to.equal(15.2);
     });
 });
