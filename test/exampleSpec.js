b886afbc28373c66fa68c3112885b41b5aabbd2f
Mathias Schreck <mat@lusini.com>
2013-10-22 17:04:56 +0200

    weird autoformat

--- test/exampleSpec.js
+++ test/exampleSpec.js
@@ -30,15 +30,18 @@ describe('KataPotter', function () {
         {
             cart: { book1: 2},
             cartTotal: 16,
-            description: 'charges 16 EURO fro two copies of the same book'},
+            description: 'charges 16 EURO fro two copies of the same book'
+        },
         {
             cart: { book1: 1, book2: 1},
             cartTotal: 15.2,
-            description: 'charges 95 % of 16 EUR for two separate books'},
+            description: 'charges 95 % of 16 EUR for two separate books'
+        },
         {
             cart: { book1: 1, book2: 1, book3: 1},
             cartTotal: 21.6,
-            description: 'charges 90 % of 24 EUR for three separate books'},
+            description: 'charges 90 % of 24 EUR for three separate books'
+        },
         {
             cart: { book1: 2, book2: 2},
             cartTotal: 30.4,
