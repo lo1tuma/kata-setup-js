817788b90564955a67c4d0c140ac485d5b705a54
Mathias Schreck <mat@lusini.com>
2013-10-22 17:04:25 +0200

    parameterized test

--- test/exampleSpec.js
+++ test/exampleSpec.js
@@ -14,30 +14,41 @@ function priceFor(cart) {
             3: .9,
             2: .95,
             1: 1
-        };
+        },
+        numberOfTotalItems = sum(cart);
 
-    numberOfTotalItems = sum(cart);
     return discountFactorFor[differentProducts] * defaultBookPrice * numberOfTotalItems;
 }
 
 describe('KataPotter', function () {
-    it('charges 8 EUR for a single book', function () {
-        expect(priceFor({ book1: 1})).to.equal(8);
-    });
+    var testData = [
+        {
+            cart: { book1: 1},
+            cartTotal: 8,
+            description: 'charges 8 EUR for a single book'
+        },
+        {
+            cart: { book1: 2},
+            cartTotal: 16,
+            description: 'charges 16 EURO fro two copies of the same book'},
+        {
+            cart: { book1: 1, book2: 1},
+            cartTotal: 15.2,
+            description: 'charges 95 % of 16 EUR for two separate books'},
+        {
+            cart: { book1: 1, book2: 1, book3: 1},
+            cartTotal: 21.6,
+            description: 'charges 90 % of 24 EUR for three separate books'},
+        {
+            cart: { book1: 2, book2: 2},
+            cartTotal: 30.4,
+            description: 'charges 95 % of 32 EUR for two copies of two separate books'
+        }
+    ];
 
-    it('charges 16 EURO fro two copies of the same book', function () {
-        expect(priceFor({book1: 2})).to.equal(16);
-    });
-
-    it('charges 95 % of 16 EUR for two separate books', function () {
-        expect(priceFor({book1: 1, book2: 1})).to.equal(15.2);
-    });
-
-    it('charges 90 % of 24 EUR for three separate books', function () {
-        expect(priceFor({book1: 1, book2: 1, book3: 1})).to.equal(21.6);
-    });
-
-    it('charges 95 % of 32 EUR for two copies of two separate books', function () {
-        expect(priceFor({book1: 2, book2: 2})).to.equal(30.4);
+    testData.forEach(function (data) {
+        it(data.description, function () {
+            expect(priceFor(data.cart)).to.equal(data.cartTotal);
+        });
     });
 });
