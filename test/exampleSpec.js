f68e64a6994865afa261819d4ee273629750d859
Mathias Schreck <mat@lusini.com>
2013-10-22 16:30:16 +0200

    added second discount

--- test/exampleSpec.js
+++ test/exampleSpec.js
@@ -1,13 +1,18 @@
 var expect = require('chai').expect;
 
+
 function priceFor(cart) {
     var differentCategories = Object.keys(cart).length,
-        discountFor2Products = 0.95;
+        discountFor2Products = 0.95,
+        discountFor3Products = 0.9,
+        defaultBookPrice = 8;
 
-    if(differentCategories > 1) {
-        return discountFor2Products * (2 * 8);
+    if(differentCategories === 3) {
+        return discountFor3Products * (3 * defaultBookPrice);
+    } else if(differentCategories === 2) {
+        return discountFor2Products * (2 * defaultBookPrice);
     } else {
-        return cart.book1 * 8;
+        return cart.book1 * defaultBookPrice;
     }
 }
 
@@ -22,5 +27,9 @@ describe('KataPotter', function () {
 
     it('charges 95 % of 16 EUR for two separate books', function () {
         expect(priceFor({book1: 1, book2: 1})).to.equal(15.2);
+    });
+
+    it('charges 90 % of 24 EUR for three separate books', function () {
+        expect(priceFor({book1: 1, book2: 1, book3: 1})).to.equal(21.6);
     });
 });
